156e990d7d2321bf8aa24d0b513697f3ad5960ad
Ravshan <eminemravshan@gmail.com>
2026-09-19 23:14:23 +0500

    Use environment variables for admin password reset

--- main/management/commands/reset_admin_password.py
+++ main/management/commands/reset_admin_password.py
@@ -1,19 +1,25 @@
+import os
+
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
-    help = "Admin parolini yangilash"
-
-    def add_arguments(self, parser):
-        parser.add_argument("username")
-        parser.add_argument("password")
+    help = "Admin parolini environment variable orqali yangilash"
 
     def handle(self, *args, **options):
-        User = get_user_model()
+        username = os.environ.get("ADMIN_USERNAME")
+        password = os.environ.get("ADMIN_PASSWORD")
 
-        username = options["username"]
-        password = options["password"]
+        if not username or not password:
+            self.stdout.write(
+                self.style.ERROR(
+                    "ADMIN_USERNAME yoki ADMIN_PASSWORD topilmadi."
+                )
+            )
+            return
+
+        User = get_user_model()
 
         try:
             user = User.objects.get(username=username)
@@ -27,9 +33,8 @@ class Command(BaseCommand):
         user.save()
 
         self.stdout.write(
-            self.style.SUCCESS("Parol muvaffaqiyatli yangilandi.")
+            self.style.SUCCESS("Admin paroli muvaffaqiyatli yangilandi.")
         )
-
 
 
 
